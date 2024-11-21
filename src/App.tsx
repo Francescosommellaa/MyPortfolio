24a96352ba744a7d30c7871e03cdc7630dfca64c
Francescosommellaa <franc.sommella@gmail.com>
2024-11-21 02:21:18 +0100

    Fix(navbar): Changed import in in the navbar to fix a bug where it was giving me an error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
 
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
@@ -11,17 +12,20 @@ import Progetto from "./pages/Progetto/Progetto";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/lavori" element={<Lavori />} />
-        <Route path="/parliamo" element={<Parliamo />} />
-        <Route path="/progetto/:id" element={<Progetto />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <>
+      <CustomCursor />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/lavori" element={<Lavori />} />
+          <Route path="/parliamo" element={<Parliamo />} />
+          <Route path="/progetto/:id" element={<Progetto />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 };
 
