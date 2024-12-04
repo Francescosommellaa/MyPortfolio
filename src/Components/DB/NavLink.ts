8d7a522b19a99834c62314d116404b1a000602ba
Francesco Sommella <francescosommellaa@gmail.com>
2024-12-04 10:59:57 +0100

    fix( created db for nav link) I made sure to put the repetitive information in a db and put it in the nav bar with a map so that it could be inserted quickly.
    Also changed some dimensions and general rules

---
+++ src/Components/DB/NavLink.ts
@@ -0,0 +1,12 @@
+const NavLink: { name: string; url: string }[] = [
+    {
+        name: "About",
+        url: "/about"
+    },
+    {
+        name: "Parliamo",
+        url: "/parliamo"
+    }
+];
+
+export default NavLink;
--- src/Components/Molecules/Navbar/Navbar.tsx
+++ src/Components/Molecules/Navbar/Navbar.tsx
@@ -2,39 +2,33 @@ import React from "react";
 import "./Navbar.scss";
 
 import { Link, useLocation } from "react-router-dom";
+import NavLink from "../../DB/NavLink";
+
+// Atoms
+import Logo from "../../Atoms/Logo/Logo";
 
 const Navbar: React.FC = () => {
   const location = useLocation();
 
   return (
     <nav className="navbar">
-      <div className="logo-container">
-        <Link to="/">
-          <h5 className="logo-text">© Design by Fra | © Francesco Sommella</h5>
-        </Link>
-      </div>
+      <Link to="/">
+        <Logo size={"S"} />
+      </Link>
 
       <ul className="nav-links">
-        <li>
-          <Link
-            to="/about"
-            className={`nav-link ${
-              location.pathname === "/about" ? "active" : ""
-            }`}
-          >
-            <h5>About</h5>
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/parliamo"
-            className={`nav-link ${
-              location.pathname === "/parliamo" ? "active" : ""
-            }`}
-          >
-            <h5>Parliamo</h5>
-          </Link>
-        </li>
+        {NavLink.map((link) => (
+          <li key={link.name}>
+            <Link
+              to={link.url}
+              className={`nav-link ${
+                location.pathname === link.url ? "active" : ""
+              }`}
+            >
+              <span>{link.name}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
