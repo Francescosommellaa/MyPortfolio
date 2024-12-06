a6ed519b3d08fcdea555bd68988ee34c5106213f
Francesco Sommella <francescosommellaa@gmail.com>
2024-12-06 08:55:49 +0100

    Feat( created work molecule and inserted into home)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,11 +3,13 @@ import "./Home.scss";
 
 // Molecules
 import Hero from "../../Components/Molecules/Hero/Hero";
+import Work from "../../Components/Molecules/Work/Work";
 
 const Home: React.FC = () => {
   return (
     <>
       <Hero />
+      <Work />
       <div className="home">
         <h1>Home Page</h1>
       </div>
