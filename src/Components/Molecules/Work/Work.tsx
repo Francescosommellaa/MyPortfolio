94c318446dd3f50dff833668b83cfca95c02b47a
Francesco Sommella <francescosommellaa@gmail.com>
2024-12-09 11:41:50 +0100

    fix(insert tabs in the work section)

--- src/Components/Molecules/Work/Work.tsx
+++ src/Components/Molecules/Work/Work.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Work.scss";
 
+// Atoms
+import WorkCards from "../../Atoms/WorkCards/WorkCards";
+
 const Work: React.FC = () => {
   return (
-    <div className="work">
-      <h1>Work</h1>
-    </div>
+    <section className="work-container">
+      {/* work cards */}
+      <h1 className="section-title">Lavori</h1>
+      <WorkCards />
+    </section>
   );
 };
 
