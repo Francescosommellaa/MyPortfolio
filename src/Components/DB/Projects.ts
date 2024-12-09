28fe918eb0462890b7e467267fc37c99232f6035
Francesco Sommella <francescosommellaa@gmail.com>
2024-12-09 11:40:18 +0100

    fix(Optimized naming for projects).

---
+++ src/Components/DB/Projects.ts
@@ -0,0 +1,67 @@
+const Projects: {
+    slug: string;
+    Title: string;
+    WesiteUrl: string;
+    Url: string;
+    DateOfPublication: string;
+    CreatorName: string;
+    Services: string;
+    Description: string;
+    ProjectCover: string;
+    ProjectImg: string;
+}[] = [
+        {
+            slug: "boolzapp",
+            Title: "Boolzapp",
+            Url: "/Boolzapp",
+            WesiteUrl: "https://github.com/Francescosommellaa/vue-boolzapp",
+            DateOfPublication: "07/2024",
+            CreatorName: "Francesco Sommella",
+            Services: "Front-end Developer",
+            Description:
+                "Un'applicazione Vue.js ispirata a WhatsApp per inviare messaggi.",
+            ProjectCover: "VueBoolzapp.jpg",
+            ProjectImg: "",
+        },
+        {
+            slug: "element-gaming",
+            Title: "Element Gaming",
+            Url: "/Element-Gaming",
+            WesiteUrl: "https://element-gaming.eu/",
+            DateOfPublication: "07/2023",
+            CreatorName: "Francesco Sommella",
+            Services: "UX/UI Design",
+            Description:
+                "Una webapp per lo streaming di videogiochi.",
+            ProjectCover: "VueBoolzapp.jpg",
+            ProjectImg: "",
+        },
+        {
+            slug: "morra-cinese",
+            Title: "Morra Cinese",
+            Url: "/MorraCinese",
+            WesiteUrl: "https://github.com/Francescosommellaa/MorraCinese",
+            DateOfPublication: "10/2024",
+            CreatorName: "Francesco Sommella",
+            Services: "Front-end Developer",
+            Description:
+                "Porta l'iconica sfida della Morra Cinese a un livello completamente nuovo! Con un'estetica audace e futuristica ispirata allo stile energico di Rocket League.",
+            ProjectCover: "MorraCinese.jpg",
+            ProjectImg: "",
+        },
+        {
+            slug: "todo-list",
+            Title: "Todo List",
+            Url: "/todolist",
+            WesiteUrl: "https://github.com/Francescosommellaa/vue-todolist",
+            DateOfPublication: "06/2024",
+            CreatorName: "Francesco Sommella",
+            Services: "Front-end Developer",
+            Description:
+                "Un'app di gestione delle attività sviluppata con Vue.js per organizzare e monitorare le attività quotidiane.",
+            ProjectCover: "VueTodolist.jpg",
+            ProjectImg: "",
+        },
+    ];
+
+export default Projects;
