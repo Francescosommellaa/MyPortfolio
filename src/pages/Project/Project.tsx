2016ecfd1b296dcfa2cc12bbdaa2b70eea7a528f
Francesco Sommella <francescosommellaa@gmail.com>
2024-12-11 09:29:55 +0100

    Fix(dynamic projects enabled) now when you click on a project in addition to entering the project page you have access to all the project data

--- src/pages/Project/Project.tsx
+++ src/pages/Project/Project.tsx
@@ -1,14 +1,33 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+// DB
+import Projects from "../../Components/DB/Projects";
+
 // SCSS
 import "./Project.scss";
 
 const Project: React.FC = () => {
-  const { slug } = useParams();
+  const { slug } = useParams<{ slug: string }>();
+  const project = Projects.find((p) => p.slug === slug);
+
+  if (!project) {
+    return <h1>Progetto non trovato</h1>;
+  }
+  const { Title, Url, Date, CreatorName, Services, Description, ProjectImg } =
+    project;
+
   return (
     <main>
-      <h1>Dettagli del Progetto {slug}</h1>
+      {/* Carosello IMG*/}
+
+      {/* Titolo */}
+      <h1>Dettagli del Progetto {Title}</h1>
+      {/* link al sito */}
+
+      {/* Descrizione */}
+
+      {/* altre info */}
     </main>
   );
 };
