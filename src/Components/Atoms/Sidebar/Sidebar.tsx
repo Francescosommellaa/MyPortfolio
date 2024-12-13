4761288d74bac7ce45b58f180ceabe5a22163581
Francesco Sommella <francescosommellaa@gmail.com>
2024-12-13 11:34:31 +0100

    Feat(created full sidebar + responsive)

---
+++ src/Components/Atoms/Sidebar/Sidebar.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+// DB
+import NavLink from "../../DB/NavLink";
+import SocialLinks from "../../DB/Social";
+
+// SCSS
+import "./Sidebar.scss";
+
+// Atoms
+import Logo from "../Logo/Logo";
+
+const menuIcon = new URL(
+  "/src/Assets/Icon-Tools/Icon/Name=menu, Dimension=M.svg",
+  import.meta.url
+).href;
+const closeIcon = new URL(
+  "/src/Assets/Icon-Tools/Icon/Name=Close, Dimension=M.svg",
+  import.meta.url
+).href;
+
+const Sidebar: React.FC = () => {
+  const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="sidebar-container">
+      {/* Icona del menu */}
+      <a className="menu-icon" onClick={toggleSidebar}>
+        <img src={!isOpen ? menuIcon : closeIcon} alt="Close menu" />
+      </a>
+
+      {/* Sidebar */}
+      <div className={`sidebar ${isOpen ? "open" : ""}`}>
+        <div className="sidebar-header">
+          <Link to="/" onClick={toggleSidebar}>
+            <Logo size={"S"} />
+          </Link>
+        </div>
+
+        {/* Navigazione */}
+        <nav className="sidebar-nav">
+          <Link
+            to="/"
+            onClick={toggleSidebar}
+            className={`nav-link ${
+              location.pathname === "/Home" ? "active" : ""
+            }`}
+          >
+            <h4>Home</h4>
+          </Link>
+          {NavLink.map((link) => (
+            <Link
+              key={link.name}
+              to={link.url}
+              onClick={toggleSidebar}
+              className={`nav-link ${
+                location.pathname === link.url ? "active" : ""
+              }`}
+            >
+              <h4>{link.name}</h4>
+            </Link>
+          ))}
+        </nav>
+
+        {/* Links sociali */}
+        <footer className="sidebar-footer">
+          {SocialLinks.map((social, index) => (
+            <a
+              className="text-paragraph-small"
+              href={social.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={index}
+            >
+              {social.name}
+            </a>
+          ))}
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
