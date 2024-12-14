311d4af8e52939b73b14a6e2e7f21b5761deacbd
Francesco Sommella <francescosommellaa@gmail.com>
2024-12-14 10:40:23 +0100

    Fix(deleted scss from home and added Hook useSize to make the page responsive)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-
-// SCSS
-import "./Home.scss";
+import { useMediaQuery } from "react-responsive";
 
 // Molecules
 import Hero from "../../Components/Molecules/Hero/Hero";
@@ -9,11 +7,12 @@ import Work from "../../Components/Molecules/Work/Work";
 import Tools from "../../Components/Molecules/Tools/Tools";
 
 const Home: React.FC = () => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   return (
     <main>
       <Hero />
       <Work />
-      <Tools size="M" />
+      <Tools size={isMobile ? "S" : "M"} />
     </main>
   );
 };
