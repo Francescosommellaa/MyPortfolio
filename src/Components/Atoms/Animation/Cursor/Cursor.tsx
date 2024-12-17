d3da0f94568a1779d95b31d4277c2341b20cc0be
Francesco Sommella <francescosommellaa@gmail.com>
2024-12-17 10:23:31 +0100

    Feat(Cursor v1.0)

---
+++ src/Components/Atoms/Animation/Cursor/Cursor.tsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+
+// SCSS
+import "./Cursor.scss";
+
+interface cursorprops {
+  light?: boolean;
+}
+
+const Cursor: React.FC<cursorprops> = ({ light = false }) => {
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [isAbsorbed, setIsAbsorbed] = useState(false);
+  const [isInput, setIsInput] = useState(false);
+  const [isText, setIsText] = useState(false);
+  const [textHeight, setTextHeight] = useState(24);
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      setCursorPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    const handleLinkHover = () => setIsAbsorbed(true);
+    const handleLinkLeave = () => setIsAbsorbed(false);
+    const handleInputHover = () => setIsInput(true);
+    const handleInputLeave = () => setIsInput(false);
+    const handleTextHover = (e: Event) => {
+      setIsText(true);
+      const element = e.target as HTMLElement;
+      const computedStyle = window.getComputedStyle(element);
+      const lineHeight = parseFloat(computedStyle.lineHeight);
+      setTextHeight(lineHeight);
+    };
+    const handleTextLeave = () => setIsText(false);
+
+    // Select all links and buttons
+    const interactiveElements = document.querySelectorAll("a, button");
+    const inputElements = document.querySelectorAll("input, textarea");
+    const textElements = document.querySelectorAll(
+      "p,span, h1, h2, h3, h4, h5, h6"
+    );
+
+    // Add event listeners to all interactive elements
+    interactiveElements.forEach((element) => {
+      element.addEventListener("mouseenter", handleLinkHover);
+      element.addEventListener("mouseleave", handleLinkLeave);
+    });
+
+    inputElements.forEach((element) => {
+      element.addEventListener("mouseenter", handleInputHover);
+      element.addEventListener("mouseleave", handleInputLeave);
+    });
+
+    textElements.forEach((element) => {
+      element.addEventListener("mouseenter", handleTextHover);
+      element.addEventListener("mouseleave", handleTextLeave);
+    });
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    // Cleanup
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      interactiveElements.forEach((element) => {
+        element.removeEventListener("mouseenter", handleLinkHover);
+        element.removeEventListener("mouseleave", handleLinkLeave);
+      });
+      inputElements.forEach((element) => {
+        element.removeEventListener("mouseenter", handleInputHover);
+        element.removeEventListener("mouseleave", handleInputLeave);
+      });
+      textElements.forEach((element) => {
+        element.removeEventListener("mouseenter", handleTextHover);
+        element.removeEventListener("mouseleave", handleTextLeave);
+      });
+    };
+  }, []);
+
+  return (
+    <div
+      className={`custom-cursor ${isAbsorbed ? "absorbed" : ""} ${
+        light ? "light" : ""
+      } ${isInput ? "input-cursor" : ""} ${isText ? "text-cursor" : ""}`}
+      style={{
+        left: `${cursorPosition.x}px`,
+        top: `${cursorPosition.y}px`,
+        height: isText ? `${textHeight}px` : undefined,
+      }}
+    />
+  );
+};
+
+export default Cursor;
